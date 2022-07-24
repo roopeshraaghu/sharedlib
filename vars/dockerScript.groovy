22328e86b21e0953d020af213e5f79650d509b16
roopeshraaghu <101583281+roopeshraaghu@users.noreply.github.com>
2022-07-24 14:37:27 +0530

    Update dockerScript.groovy

--- vars/dockerScript.groovy
+++ vars/dockerScript.groovy
@@ -14,14 +14,14 @@ environment {
 	
 	stages {
 		stage("POLL SCM"){
-      			agent{label 'docker'}
+      			agent{label 'dock'}
 			steps {
 				 checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCredId", url: "$gitRepo"]]])
 			}
 		}	
 					
 		stage('BUILD IMAGE') {
-       			agent{label 'docker'}
+       			agent{label 'dock'}
 			steps { 
 				script { 
 					dockerimage = docker.build registry + ":$dockerTag" 
@@ -30,7 +30,7 @@ environment {
 		}
 					
 		stage('PUSH HUB') { 
-       			agent{label 'docker'}
+       			agent{label 'dock'}
 			steps { 
 				script {
 					docker.withRegistry( '', registryCredential ) { 
